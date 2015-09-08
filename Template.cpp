47b20ea579c3b4bf37d1b6e3fe5537bf020c3f42
santanu-thangaraj <t.santanu@gmail.com>
2015-09-08 21:17:54 +0530

    commiting the V1 merge code

---
+++ Template.cpp
@@ -0,0 +1,352 @@
+#include <CL/cl.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <time.h>
+#include <string>
+#include <fstream>
+#include <iostream>
+
+#define ENABLE_DEBUG_PRINT
+
+cl_context          context;
+cl_kernel			kernel_leaf;              // compute kernel_leaf
+cl_kernel		    kernel_merge;             // compute kernel_merge
+
+template <class T>
+class MergeSortData {
+public:
+	T* elems;     // elements 
+	int length;
+	cl_mem cl_array1, cl_array2;
+
+	MergeSortData(int);
+	void Merge(T[], int , int, int);  
+	void MergeSort(T[], int, int);
+	void Get_GPU_op(void);
+};
+
+template <class T>
+MergeSortData<T>::Get_GPU_op()
+{
+	//set kernel arguments to the kernel
+
+	// the input array to the kernel
+	status = clSetKernelArg(
+		kernel,
+		1,
+		sizeof(cl_mem),
+		(void *)&inputBuffer);
+	if (status != CL_SUCCESS)
+	{
+		std::cout << "Error: Setting kernel argument. (input)\n";
+		return SDK_FAILURE;
+	}
+}
+
+template <class T>
+MergeSortData<T>::MergeSortData(int len)
+{
+	int i, status;
+	/*constructor for initializing the input array*/
+	length = len;
+	elems = new T[length];
+#ifdef	ENABLE_DEBUG_PRINT
+	std::cout << "The input array is:\n";
+#endif	
+	for (i = 0; i < length; i++)
+	{
+		elems[i] = rand() % 10000;
+#ifdef	ENABLE_DEBUG_PRINT
+		std::cout << elems[i]<<"\t";
+#endif
+	}
+
+	cl_array1 = clCreateBuffer(
+		context,
+		CL_MEM_READ_WRITE,
+		sizeof(T) * length,
+		NULL,
+		&status);
+
+	if (status != CL_SUCCESS)
+	{
+		std::cout << "Error: clCreateBuffer (cl_array1)\n";
+	}
+
+	cl_array2 = clCreateBuffer(
+		context,
+		CL_MEM_READ_WRITE,
+		sizeof(T) * length,
+		NULL,
+		&status);
+
+	if (status != CL_SUCCESS)
+	{
+		std::cout << "Error: clCreateBuffer (cl_array2)\n";
+	}
+};
+
+template <class T>
+void MergeSortData<T>::Merge(T tempArray[], int start, int mid, int end)
+{
+	T leftArray[5001];
+	T rightArray[5001];
+	int n1 = mid - start + 1;
+	int n2 = end - mid;
+	int i, j, k;
+
+	for (i = 0; i < n1; i++)
+	{
+		leftArray[i] = tempArray[start + i];
+	}
+	for (j = 0; j < n2; j++)
+	{
+		rightArray[j] = tempArray[mid + j + 1];
+	}
+	// Assign the Sentinel value
+	leftArray[i] = 99999;
+	rightArray[j] = 99999;
+	i = 0;
+	j = 0;
+	for (k = start; k <= end; k++)
+	{
+		if (leftArray[i] <= rightArray[j])
+		{
+			tempArray[k] = leftArray[i];
+			i = i + 1;
+		}
+		else
+		{
+			tempArray[k] = rightArray[j];
+			j = j + 1;
+		}
+	}
+}
+
+template <class T>
+void MergeSortData<T>::MergeSort(T tempArray[], int start, int end)
+{
+	int mid;
+	if (start < end)
+	{
+		mid = (start + end) / 2;
+		MergeSort(tempArray, start, mid);
+		MergeSort(tempArray, (mid + 1), end);
+		Merge(tempArray, start, mid, end);
+	}
+}
+
+/*
+* Converts the contents of a file into a string
+*/
+std::string
+convertToString(const char *filename)
+{
+	size_t size;
+	char*  str;
+	std::string s;
+
+	std::fstream f(filename, (std::fstream::in | std::fstream::binary));
+
+	if (f.is_open())
+	{
+		size_t fileSize;
+		f.seekg(0, std::fstream::end);
+		size = fileSize = (size_t)f.tellg();
+		f.seekg(0, std::fstream::beg);
+
+		str = new char[size + 1];
+		if (!str)
+		{
+			f.close();
+			std::cout << "Memory allocation failed";
+			return NULL;
+		}
+
+		f.read(str, fileSize);
+		f.close();
+		str[size] = '\0';
+
+		s = str;
+		delete[] str;
+		return s;
+	}
+	else
+	{
+		std::cout << "\nFile containg the kernel code(\".cl\") not found. Please copy the required file in the folder containg the executable.\n";
+		exit(1);
+	}
+	return NULL;
+}
+
+
+
+int main(int argc, char** argv)
+{
+	int gpu = 1; //replace it with using argument initialization
+	int err, is_double = 0;
+	int input_size;
+
+//	size_t global;                      // global domain size for our calculation
+//	size_t local;                       // local domain size for our calculation
+
+	cl_device_id device_id;             // compute device id 
+	cl_command_queue commands;          // compute command queue
+	cl_program program;                 // compute program
+	cl_uint numPlatforms;
+	cl_platform_id platform = NULL;
+
+
+
+	if (argc != 3)
+	{
+		//std::cout << "Usage is: executable.exe float/double input_array_size";
+		//return EXIT_FAILURE;
+		is_double = 1;
+		input_size = 24;
+	}
+	else
+	{
+		if (strcmp("double", "argv[1]") == 0)
+		{
+			is_double = 1;
+		}
+
+		input_size = atoi(argv[2]);
+	}
+
+	err = clGetPlatformIDs(0, NULL, &numPlatforms);
+	if (err != CL_SUCCESS)
+	{
+		std::cout << "Error: Getting Platforms. (clGetPlatformsIDs)\n";
+
+	}
+
+	if (numPlatforms > 0)
+	{
+		cl_platform_id* platforms = new cl_platform_id[numPlatforms];
+		err = clGetPlatformIDs(numPlatforms, platforms, NULL);
+		if (err != CL_SUCCESS)
+		{
+			std::cout << "Error: Getting Platform Ids. (clGetPlatformsIDs)\n";
+
+		}
+		for (unsigned int i = 0; i < numPlatforms; ++i)
+		{
+			char pbuff[100];
+			err = clGetPlatformInfo(
+				platforms[i],
+				CL_PLATFORM_VENDOR,
+				sizeof(pbuff),
+				pbuff,
+				NULL);
+			if (err != CL_SUCCESS)
+			{
+				std::cout << "Error: Getting Platform Info.(clGetPlatformInfo)\n";
+			}
+			platform = platforms[i];
+			if (!strcmp(pbuff, "Advanced Micro Devices, Inc."))
+			{
+				break;
+			}
+		}
+		delete platforms;
+	}
+
+	if (NULL == platform)
+	{
+		std::cout << "NULL platform found so Exiting Application." << std::endl;
+	}
+
+	err = clGetDeviceIDs(platform, gpu ? CL_DEVICE_TYPE_GPU : CL_DEVICE_TYPE_CPU, 1, &device_id, NULL);
+	if (err != CL_SUCCESS)
+	{
+		std::cout << "Error: Failed to create a device group!\n";
+		return EXIT_FAILURE;
+	}
+
+	// Create a compute context 
+	//
+	context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
+	if (!context)
+	{
+		std::cout << "Error: Failed to create a compute context!\n";
+		return EXIT_FAILURE;
+	}
+	// Create a command commands
+	//
+	commands = clCreateCommandQueue(context, device_id, 0, &err);
+	if (!commands)
+	{
+		std::cout << "Error: Failed to create a command commands!\n";
+		return EXIT_FAILURE;
+	}
+
+
+	//Building Kernel
+	const char * filename = "Template_Kernels.cl";
+	std::string  sourceStr = convertToString(filename);
+	const char * source = sourceStr.c_str();
+	size_t sourceSize[] = { strlen(source) };
+
+	program = clCreateProgramWithSource(
+		context,
+		1,
+		&source,
+		sourceSize,
+		&err);
+	if (err != CL_SUCCESS)
+	{
+		std::cout <<
+			"Error: Loading Binary into cl_program \
+               (clCreateProgramWithBinary)\n";
+	}
+
+	// Build the program executable
+	//
+	if (is_double)
+	{
+		err = clBuildProgram(program, 0, NULL, "-D DOUBLE", NULL, NULL);
+	}
+	else
+	{
+		err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+	}
+	if (err != CL_SUCCESS)
+	{
+		size_t len;
+		char buffer[2048];
+
+		std::cout << "Error: Failed to build program executable!\n";
+		clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer, &len);
+		std::cout << "%s\n",buffer;
+		return EXIT_FAILURE;
+	}
+
+	// Create the compute kernel in the program we wish to run
+	//
+	kernel_leaf = clCreateKernel(program, "parallel_insertion_sort_leaf", &err);
+	if (!kernel_leaf || err != CL_SUCCESS)
+	{
+		std::cout << "Error: Failed to create compute kernel_leaf!\n";
+		return EXIT_FAILURE;
+	}
+
+	kernel_merge = clCreateKernel(program, "merge", &err);
+	if (!kernel_merge || err != CL_SUCCESS)
+	{
+		std::cout << "Error: Failed to create compute kernel_leaf!\n";
+		return EXIT_FAILURE;
+	}
+
+	if (is_double)
+	{
+		MergeSortData<double> merge_class_data(input_size);
+
+	}
+	else
+	{
+		MergeSortData<float> merge_class_data(input_size);
+	}
+
+}
---
+++ Template_Kernels.cl
@@ -0,0 +1,137 @@
+/*
+This is a parallel merge sort code
+*/
+
+#define WGSIZE 256
+
+#ifdef DOUBLE
+#define FPTYPE double
+
+#ifdef cl_khr_fp64
+#pragma OPENCL EXTENSION cl_khr_fp64 : enable
+#elif defined(cl_amd_fp64)
+#pragma OPENCL EXTENSION cl_amd_fp64 : enable
+#else
+#error "Double precision floating point not supported by OpenCL implementation."
+#endif
+#else
+#define FPTYPE float
+#endif
+
+kernel void parallel_insertion_sort_leaf(
+	__global FPTYPE* g_array,
+	int final_blk_size
+	)
+{
+	int g_id = get_global_id(0);
+	int grp_id = get_group_id(0);
+	int grp_size = get_num_groups(0);
+	int l_id = get_local_id(0);
+	//	int l_size = get_local_size(0);
+	__local FPTYPE l_array[WGSIZE];
+	int iter;
+	int final_idx, repeat_cnt;
+	int local_blk_size = WGSIZE;
+	FPTYPE my_element;
+
+	if (grp_id == (grp_size - 1))
+	{
+		local_blk_size = final_blk_size;
+	}
+
+	if (l_id < local_blk_size)
+	{
+		my_element = g_array[g_id];
+		l_array[l_id] = my_element;
+	}
+
+	barrier(CLK_LOCAL_MEM_FENCE);
+
+	if (l_id >= local_blk_size) return;
+
+	final_idx = 0;
+	repeat_cnt = 0;
+	for (iter = 0; iter < local_blk_size; iter++)
+	{
+		final_idx += (l_array[iter] < my_element);
+		repeat_cnt += (l_array[iter] == my_element);
+	}
+
+	//store the results
+
+	final_idx += grp_id * grp_size;
+	g_array[final_idx] = my_element;
+	for (iter = 0; iter < repeat_cnt; iter++)
+	{
+		g_array[++final_idx] = my_element;
+	}
+}
+
+kernel void merge(
+	__global FPTYPE* g_array_inp,
+	__global FPTYPE* g_array_op,
+	int array_size,
+	int last_array_size,
+	int tot_thread_needed
+	)
+{
+	int g_id = get_global_id(0);
+	int my_start_idx_1 = g_id * (array_size * 2);
+	int my_start_idx_2 = my_start_idx_1 + array_size;
+	int op_idx = my_start_idx_1;
+	int array_1_len = array_size;
+	int array_2_len = array_size;
+	int iter;
+	int my_idx_1, my_idx_2;
+
+	FPTYPE array1_element, array2_element;
+
+	if (g_id == (tot_thread_needed - 1))
+	{
+		array_2_len = last_array_size;
+	}
+
+	my_idx_1 = my_start_idx_1;
+	my_idx_2 = my_start_idx_2;
+
+	array1_element = g_array_inp[my_idx_1++];
+	array2_element = g_array_inp[my_idx_2++];
+
+	for (iter = 0; iter < (array_1_len + array_2_len); iter++)
+	{
+		if (array1_element < array2_element)
+		{
+			g_array_op[op_idx++] = array1_element;
+
+			if (my_idx_1 < my_start_idx_2)
+			{
+				array1_element = g_array_inp[my_idx_1++];
+			}
+			else
+			{
+				for (; iter < (array_1_len + array_2_len); iter++)
+				{
+					g_array_op[op_idx++] = g_array_inp[my_idx_2++];
+				}
+				break;
+			}
+		}
+		else
+		{
+			g_array_op[op_idx++] = array2_element;
+			if (my_idx_2 < (my_start_idx_2 + array_2_len))
+			{
+				array2_element = g_array_inp[my_idx_2++];
+			}
+			else
+			{
+				for (; iter < (array_1_len + array_2_len); iter++)
+				{
+					g_array_op[op_idx++] = g_array_inp[my_idx_1++];
+				}
+				break;
+			}
+		}
+
+	}
+}
